8f5817068d22446b276494a0f099ca8bbe7d6cb5
veepee <66334129+vpontual@users.noreply.github.com>
2024-04-10 17:52:27 -0400

    updated product-routes with corrected routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,23 @@ const { Product, Category, Tag, ProductTag } = require("../../models");
 router.get("/", (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [Category, Tag],
+  })
+    .then((products) => res.json(products))
+    .catch((err) => res.status(500).json(err));
 });
 
 // get one product
 router.get("/:id", (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: { id: req.params.id },
+    include: [Category, Tag],
+  })
+    .then((product) => res.json(product))
+    .catch((err) => res.status(400).json(err));
 });
 
 // create new product
@@ -93,6 +104,11 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: { id: req.params.id },
+  })
+    .then((product) => res.json(product))
+    .catch((err) => res.status(400).json(err));
 });
 
 module.exports = router;
